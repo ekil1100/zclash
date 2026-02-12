15ca3378a20566d2c34b1c9bc24f370d94c1ceb7
Like <like@ekil.sh>
2026-02-13 02:09:58 +0800

    ci: update release workflow and homebrew formula for binary distribution
    - Rename zclash to zc in release workflow
    
    - Update homebrew formula to use prebuilt binaries
    
    - Remove zig build dependency for homebrew installs

--- homebrew-zc/zc.rb
+++ homebrew-zc/zc.rb
@@ -1,15 +1,29 @@
 class Zc < Formula
   desc "Lightweight network tool built with Zig"
   homepage "https://github.com/ekil1100/zc"
-  url "https://github.com/ekil1100/zc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  version "1.0.0"
   license "MIT"
 
-  depends_on "zig" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/ekil1100/zc/releases/download/v#{version}/zc-v#{version}-macos-arm64.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+    end
+    on_intel do
+      url "https://github.com/ekil1100/zc/releases/download/v#{version}/zc-v#{version}-macos-amd64.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_MACOS_AMD64"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/ekil1100/zc/releases/download/v#{version}/zc-v#{version}-linux-amd64.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+    end
+  end
 
   def install
-    system "zig", "build"
-    bin.install "zig-out/bin/zc"
+    bin.install "zc"
   end
 
   test do
