47dc9759ef904704cf369bb7991ba15d6f5d146b
Like <like@ekil.sh>
2026-02-13 09:33:37 +0800

    homebrew: update formula SHA256 for v1.0.0-rc1
    - Direct binary download, no build required
    
    - Support macOS arm64 and Linux amd64

--- homebrew-zc/zc.rb
+++ homebrew-zc/zc.rb
@@ -1,13 +1,13 @@
 class Zc < Formula
   desc "Lightweight network tool built with Zig"
   homepage "https://github.com/ekil1100/zc"
-  version "1.0.0"
+  version "1.0.0-rc1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ekil1100/zc/releases/download/v#{version}/zc-v#{version}-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      sha256 "c704101c409af5ba562c814db2762826ee47962833c684a1beae21602d3e23e2"
     end
     on_intel do
       url "https://github.com/ekil1100/zc/releases/download/v#{version}/zc-v#{version}-macos-amd64.tar.gz"
@@ -18,7 +18,7 @@ class Zc < Formula
   on_linux do
     on_intel do
       url "https://github.com/ekil1100/zc/releases/download/v#{version}/zc-v#{version}-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      sha256 "fdf5b6a70e562947af7d056d860d78107db2a420225e094ff674fa7ff6063d18"
     end
   end
 
